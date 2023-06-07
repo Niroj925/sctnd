8ec678d098d304c38e8edfe8300641e3ef95088f
Niroj925 <ngtc0925@gmail.com>
2023-06-07 18:07:39 +0545

    deploy live using metamask and alchamey hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv/config");
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY=process.env.ALCHEMY_API_KEY;
+const SEPOLIA_PRIVATE_KEY=process.env.SEPOLIA_PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.18",
+
+  networks:{
+    sepolia:{
+      url:`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      account:`${SEPOLIA_PRIVATE_KEY}`,
+      chainId:11155111
+    }
+  }
 };
